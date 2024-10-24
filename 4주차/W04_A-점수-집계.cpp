bc62cf1274d6dbaa855e486de0e80d9e4e798072
Yejin <qja086351@naver.com>
2024-10-24 23:23:17 +0900

    map으로 변경

--- 4주차/W04_A-점수-집계.cpp
+++ 4주차/W04_A-점수-집계.cpp
@@ -1,94 +1,58 @@
 #include <iostream>
-#include<algorithm>
-#include<set>
-#include<map>
-#include<vector>
+#include <vector>
+#include <map>
 using namespace std;
 
 int tc;
 
-int main() {
+int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
 	cin >> tc;
-	while (tc--) {
-		int cnt, indeok = 0, biryong = 0;
-		vector<pair<int, string>> inScore, biScore; //{������, �̸�}
-		cin >> cnt;
-	
-		for (int i = 0; i < cnt; i++) {
-			int newIn, newBi;
-			string player;
-			cin >> newIn >> newBi >> player;
+    while(tc--){
+        int cnt, indeokScore = 0, biryongScore = 0;
+        map<string, int> inPlayerScores, biPlayerScores;
+        cin >> cnt;
 
-			//�δ��� ����
-			if (newIn != indeok) {
-				int idx = -1;
-				for (int i = 0; i < inScore.size(); i++) {
-					if (inScore[i].second == player) {
-						idx = i;
-						break;
-					}
-				}
+        for(int i = 0; i< cnt; i++){
+            int newIn, newBi;
+            string player;
+            cin >> newIn >> newBi >> player;
 
-				//���� ����
-				if (idx != -1) {
-					inScore[idx].first += (newIn - indeok);
-					indeok = newIn;
-					continue;
-				}
-				//���ο� ����
-				inScore.push_back({ newIn - indeok, player });
-				indeok = newIn;
-				continue;
-			}
+            // 인덕팀 득점
+            if(newIn != indeokScore){
+                if(inPlayerScores.find(player) == inPlayerScores.end()) inPlayerScores.insert({player, newIn - indeokScore});
+                else inPlayerScores[player] += newIn - indeokScore;
+                indeokScore = newIn;
+                continue;
+            }
 
-			//����� ����
-			int idx = -1;
-			for (int i = 0; i < biScore.size(); i++) {
-				if (biScore[i].second == player) {
-					idx = i;
-					break;
-				}
-			}
+            // 비룡팀 득점
+            if(biPlayerScores.find(player) == biPlayerScores.end()) biPlayerScores.insert({player, newBi - biryongScore});
+            else biPlayerScores[player] += newBi - biryongScore;
+            biryongScore = newBi;
+        }
 
-			//���� ����
-			if (idx != -1) {
-				biScore[idx].first += (newBi - biryong);
-				biryong = newBi;
-				continue;
-			}
-			//���ο� ����
-			biScore.push_back({ newBi - biryong, player });
-			biryong = newBi;
-			continue;
-		}
-
-		//�δ��� �¸��� �δ��� �� �ִٵ����� ���
-		if (indeok > biryong) {
-			sort(inScore.begin(), inScore.end());
-			cout << inScore[inScore.size() - 1].second << " " << inScore[inScore.size() - 1].first << "\n";
-			continue;
-		}
-
-		//����� �¸��� ����� �� �ִٵ����� ���
-		sort(biScore.begin(), biScore.end());
-		cout << biScore[biScore.size() - 1].second << " " << biScore[biScore.size() - 1].first << "\n";
-
-
-	}
+        int ansScore = 0;
+        string ansPlayer = "";
+        // 인덕팀이 이긴 경우
+        if(indeokScore > biryongScore){
+            for(map<string, int>::iterator it = inPlayerScores.begin(); it != inPlayerScores.end(); it++){
+                if(ansScore > it->second) continue;
+                ansScore = it->second;
+                ansPlayer = it->first;
+            }
+        }
+        // 비룡팀이 이긴 경우
+        else {
+            for(map<string, int>::iterator it = biPlayerScores.begin(); it != biPlayerScores.end(); it++){
+                if(ansScore > it->second) continue;
+                ansScore = it->second;
+                ansPlayer = it->first;
+            }
+        }
+        cout << ansPlayer << " " << ansScore <<  "\n";
+    }
 }
-
-/*
-1
-6
-2 0 a
-5 0 a
-9 0 a
-9 4 b
-9 11 c
-9 13 d
-
-*/
